c0afdbced2e07ccb476017bd22c0ddd28de6e18a
swapnil <connect.swapnil02@gmail.com>
2020-08-19 20:22:11 +0530

    key issue solved

--- TwitterApp/app/src/main/java/com/demo/twitter/twitterapp/data/retrofit/ApiInterface.kt
+++ TwitterApp/app/src/main/java/com/demo/twitter/twitterapp/data/retrofit/ApiInterface.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Headers
 
 interface ApiInterface {
 
-    @Headers("authorization: OAuth oauth_consumer_key=\"qFQExz5t6rMBLVevvvQTxbEOO\",oauth_token=\"2560733537-ceA2rZLC0o5vlwnpgLKOCVbYZDEcr1MLuMe0NTa\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1597511859\",oauth_nonce=\"drCOAW\",oauth_version=\"1.0\",oauth_signature=\"2COry3%2BypBQ4f2C2NfA0JojCkNA%3D\"")
+    @Headers("authorization: OAuth oauth_consumer_key=\"qFQExz5t6rMBLVevvvQTxbEOO\",oauth_token=\"2560733537-ceA2rZLC0o5vlwnpgLKOCVbYZDEcr1MLuMe0NTa\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1597846445\",oauth_nonce=\"ObOoiT\",oauth_version=\"1.0\",oauth_signature=\"Q1RQcPue8c8yrAL2wGc9R4B1AQM%3D\"")
     @GET("home_timeline.json")
     suspend fun getUserFeed() : Response<List<Tweet>>
 }
